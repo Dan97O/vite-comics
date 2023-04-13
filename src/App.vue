20e7da55fb5c681cea8237e981bc694e54e1b6d1
Dan97O <dan97-oddo@live.it>
2023-04-13 16:26:39 +0200

    add banner center

--- src/App.vue
+++ src/App.vue
@@ -64,8 +64,55 @@ export default {
   <!-- /header -->
 
   <main>
-    <div class="banner_top"></div>
-    <div class="banner_center"></div>
+    <div class="banner_top">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h3>Content goes here</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="banner_center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 d-flex justify-content-around">
+            <ul>
+              <li>
+                <a href="#">
+                  <img src="./assets/img/buy-comics-digital-comics.png" alt="digital comics">
+                  <span>DIGITAL COMICS</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./assets/img/buy-comics-merchandise.png" alt="merchandise">
+                  <span>DC MERCHANDISE</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./assets/img/buy-comics-subscriptions.png" alt="subscriptions">
+                  <span>SUBSCRIPTION</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./assets/img/buy-comics-shop-locator.png" alt="shop locator">
+                  <span>COMIC SHOP LOCATOR</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="./assets/img/buy-dc-power-visa.svg" alt="power-visa">
+                  <span>DC POWER VISA</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </main>
   <!-- /main -->
@@ -115,11 +162,37 @@ main {
   .banner_top {
     height: 100px;
     background-color: black;
+    display: flex;
+    align-items: center;
+    color: white;
   }
 
   .banner_center {
     height: 125px;
     background-color: blue;
+    display: flex;
+    align-items: center;
+
+    ul {
+      list-style: none;
+      display: flex;
+      gap: 4rem;
+      margin: 0;
+
+      a {
+        text-decoration: none;
+        color: black;
+
+        img {
+          height: 50px;
+        }
+
+        span {
+          margin-left: 10px;
+          color: white;
+        }
+      }
+    }
   }
 }
 
