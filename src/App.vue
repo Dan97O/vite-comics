c064442ce6989537e6b761d5a5b37baf4ed57354
Dan97O <dan97-oddo@live.it>
2023-04-13 17:03:40 +0200

    add banner bottom

--- src/App.vue
+++ src/App.vue
@@ -113,7 +113,190 @@ export default {
         </div>
       </div>
     </div>
+    <div class="banner_bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <div class="row">
+              <div class="col-4">
+                <ul class="pt-3">
+                  <li>
+                    <a href="#">
+                      <h4>
+                        DC COMICS
+                      </h4>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Characters
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Comics
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Movies
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      TV
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Games
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Videos
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      News
+                    </a>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <a href="#">
+                      <h4>
+                        SHOP
+                      </h4>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Shop DC
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Shop DC Collectibles
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-4">
+                <ul class="pt-3">
+                  <li>
+                    <a href="#">
+                      <h4>
+                        DC
+                      </h4>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Terms Of Use
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Privacy policy (New)
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Ad Choices
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Advertising
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Jobs
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Subscriptions
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Talent Workshops
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      CPSC Certificates
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Ratings
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Shop Help
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      Contacts Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-4">
+                <ul class="pt-3">
+                  <li>
+                    <a href="#">
+                      <h4>
+                        SITES
+                      </h4>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      DC
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      MAD Megazine
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      DC Kids
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      DC Universe
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      DC Power Visa
+                    </a>
+                  </li>
+                </ul>
+              </div>
 
+            </div>
+
+
+
+
+
+          </div>
+          <div class="logo_dc col-6"></div>
+        </div>
+      </div>
+    </div>
   </main>
   <!-- /main -->
 
@@ -172,6 +355,8 @@ main {
     background-color: blue;
     display: flex;
     align-items: center;
+    position: relative;
+    z-index: 500;
 
     ul {
       list-style: none;
@@ -194,10 +379,48 @@ main {
       }
     }
   }
+
+  .banner_bottom {
+    background-image: url(./assets/img/footer-bg.jpg);
+    height: calc(100vh - 232px - 225px);
+
+    ul {
+      list-style: none;
+      padding: 0;
+
+      a {
+        text-decoration: none;
+        color: white;
+
+        &:hover {
+          color: blue;
+        }
+      }
+    }
+
+
+
+
+
+
+    .logo_dc {
+      background-image: url(./assets/img/dc-logo-bg.png);
+      height: 600px;
+      background-repeat: no-repeat;
+      margin-top: -5rem;
+      z-index: 0;
+    }
+
+  }
+
+
+
 }
 
 footer {
   height: 100px;
   background-color: greenyellow;
+  position: relative;
+  z-index: 500;
 }
 </style>
