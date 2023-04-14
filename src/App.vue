f0210f07bb228d79cf40dc8bf2de0f049618347f
Dan97O <dan97-oddo@live.it>
2023-04-14 14:22:52 +0200

    add SiteFooter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
-import SiteHeader from "./components/SiteHeader.vue";
-import SiteMain from "./components/SiteMain.vue";
+import SiteHeader from "./components/SiteHeader.vue"
+import SiteMain from "./components/SiteMain.vue"
+import SiteFooter from "./components/SiteFooter.vue"
 
 export default {
   components: {
     SiteHeader,
     SiteMain,
+    SiteFooter,
   },
   data() {
     return {
@@ -22,44 +24,8 @@ export default {
 
   <SiteMain />
   <!-- /SiteMain -->
-
-
-
-
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-6 d-flex align-items-center">
-          <button>SING-UP NOW!</button>
-        </div>
-        <!-- col-6 -->
-        <div class="col-6 d-flex align-items-center justify-content-end">
-          <h4>FOLLOW US</h4>
-          <ul class="d-flex gap-2">
-            <li>
-              <img src="./assets/img/footer-facebook.png" alt="">
-            </li>
-            <li>
-              <img src="./assets/img/footer-twitter.png" alt="">
-            </li>
-            <li>
-              <img src="./assets/img/footer-youtube.png" alt="">
-            </li>
-            <li>
-              <img src="./assets/img/footer-pinterest.png" alt="">
-            </li>
-            <li>
-              <img src="./assets/img/footer-periscope.png" alt="">
-            </li>
-          </ul>
-        </div>
-        <!-- col-6 -->
-      </div>
-      <!-- /row -->
-    </div>
-    <!-- /container -->
-  </footer>
-  <!-- /footer -->
+  <SiteFooter />
+  <!-- /SiteFooter -->
 </template>
 
 <style lang="scss">
@@ -74,28 +40,4 @@ ul {
 }
 
 /* #endregion Utilities */
-
-
-/* #region Footer */
-footer {
-  height: 100px;
-  background-color: #303030;
-  position: relative;
-  z-index: 500;
-  display: flex;
-  align-items: center;
-
-  button {
-    padding: 10px;
-    background-color: #303030;
-    border-color: #0282f9;
-    color: white;
-  }
-
-  h4 {
-    color: #0282f9;
-  }
-}
-
-/* #endregion Footer */
 </style>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  name: 'SiteFooter',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-6 d-flex align-items-center">
+          <button>SING-UP NOW!</button>
+        </div>
+        <!-- col-6 -->
+        <div class="col-6 d-flex align-items-center justify-content-end">
+          <h4>FOLLOW US</h4>
+          <ul class="d-flex gap-2">
+            <li>
+              <img src="./assets/img/footer-facebook.png" alt="">
+            </li>
+            <li>
+              <img src="./assets/img/footer-twitter.png" alt="">
+            </li>
+            <li>
+              <img src="./assets/img/footer-youtube.png" alt="">
+            </li>
+            <li>
+              <img src="./assets/img/footer-pinterest.png" alt="">
+            </li>
+            <li>
+              <img src="./assets/img/footer-periscope.png" alt="">
+            </li>
+          </ul>
+        </div>
+        <!-- col-6 -->
+      </div>
+      <!-- /row -->
+    </div>
+    <!-- /container -->
+  </footer>
+  <!-- /footer -->
+</template>
+
+<style lang="scss" scoped>
+/* #region Footer */
+footer {
+  height: 100px;
+  background-color: #303030;
+  position: relative;
+  z-index: 500;
+  display: flex;
+  align-items: center;
+
+  button {
+    padding: 10px;
+    background-color: #303030;
+    border-color: #0282f9;
+    color: white;
+  }
+
+  h4 {
+    color: #0282f9;
+  }
+}
+
+/* #endregion Footer */
+</style>
